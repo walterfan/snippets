c48f88fdadd85218e27564e50c23d33940cf5463
Walter Fan (yafan) <yafan@cisco.com>
2020-01-08 22:45:23 +0800

    add test

---
+++ cpp/exam/tiny_diagram.cpp
@@ -0,0 +1,11 @@
+#include "tiny_diagram.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <memory>
+
+
+int main(int argc, char **argv)
+{
+    printf("-- write image by gd ---");
+}
